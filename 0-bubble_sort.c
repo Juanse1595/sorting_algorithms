a0fe79256d2bc995b46540ae66b44f7a7201e41f
Nel10 <3461@holbertonschool.com>
2021-10-22 11:26:23 -0500

    file 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - reviews each element, compares them and exchanges them
+ * @array: array
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int idx, j, aux = 0;
+	int flag = 0;
+
+	if (array == NULL || size < 1)
+	{
+		return;
+	}
+	for (idx = 0; idx < size - 1; idx++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1 - idx; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				flag = 1;
+			}
+		}
+		print_array(array, size);
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+}
