cc8d2882cbb986530677b6fddd540a0972d3315d
Juanse1595 <3309@holbertonschool.com>
2021-10-25 19:00:25 +0000

    Update 3rd with partition function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,28 +1,53 @@
 #include "sort.h"
 
 /**
- */
+  * quick_sort - calls the qs function
+  * @array: array to be sorted
+  * @size: size of the array
+  */
+
 void quick_sort(int *array, size_t size)
 {
 	qs(array, array[0], array[size - 1]);
 }
-void qs(int *array, l, r)
+
+/**
+  * qs - quick sort function
+  * @array: array to be sorted
+  * @left: left edge of the array
+  * @right: right edge of the array
+  */
+
+void qs(int *array, left, right)
 {
 
 }
-int partition(int *array, l, r)
-{
-	int pivot = array[r];
-	int i = l - 1, j, temp;
 
-	for (j = l; j < pivot; j++)
+/**
+  * partition - fix the position of the elements
+  * @array: array to be sorted
+  * @left: left edge of the array
+  * @right: right edge of the array
+  * Return: the index of the pivot
+  */
+
+int partition(int *array, left, right)
+{
+	int pivot = array[right];
+	int i = left - 1, j, temp;
+
+	for (j = left; j < pivot; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-
-
+			temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
 		}
 	}
-
+	temp = array[right];
+	array[right] = array[i + 1];
+	array[i + 1] = temp;
+	return (i + 1);
 }
